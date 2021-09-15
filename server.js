ee6124d07b4811df0af6d7ee097eb6fbb7781078
GBaldez <83733139+GBaldez@users.noreply.github.com>
2021-09-14 22:55:11 -0300

    authentication server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const connectToDb = require("./src/database/database"); 
 const routes = require(`./src/routes/api`);
 var cors = require("cors");
@@ -19,7 +19,7 @@ app.get(`/`, (req, res) => {
 
 app.use(`/api`, routes);
 
-app.listen(process.env.port || port, () => {
+app.listen(port, () => {
     console.log('Servidor em execução na porta: '+ port);
 });
 
